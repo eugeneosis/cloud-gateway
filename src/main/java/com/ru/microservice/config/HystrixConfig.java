e6c76660638fc21bcb1162f766615b33161f1681
eugeneosis <fckngbld75@gmail.com>
2021-05-14 20:15:15 +0300

    change logic

---
+++ src/main/java/com/ru/microservice/config/HystrixConfig.java
@@ -0,0 +1,22 @@
+package com.ru.microservice.config;
+
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HystrixConfig extends HystrixCommand<String> {
+
+    public HystrixConfig() {
+        super(HystrixCommandGroupKey.Factory.asKey("HystrixConfigGroup"));
+    }
+
+    @Override
+    protected String run() {
+        HystrixCommandProperties.Setter()
+                .withExecutionTimeoutInMilliseconds(10000);
+        return execute();
+    }
+}
---
+++ src/main/java/com/ru/microservice/controller/FallbackServiceController.java
@@ -0,0 +1,19 @@
+package com.ru.microservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackServiceController {
+
+    @GetMapping("/botservicefallback")
+    public Mono<String> botServiceFallbackMethod() {
+        return Mono.just("Сервис временно не доступен");
+    }
+
+    @GetMapping("/uirestservicefallback")
+    public Mono<String> uiRestServiceFallbackMethod() {
+        return Mono.just("Сервис временно не доступен");
+    }
+}
